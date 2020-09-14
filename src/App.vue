f51016de3c0662e6e05b92f09d4dd962da0bbafd
Anna Arkhipova <qywy1234@gmail.com>
2020-09-15 00:51:15 +1000

    refactoring & fix bugs

--- src/App.vue
+++ src/App.vue
@@ -1,80 +1,68 @@
 <template>
   <div id="app">
-    <div class="wrapper">
-      <div class="app__container-title">
-        <p class="app__title">Simon says</p>
-      </div>
-      <div class="container">
-        <div class="container__btn">
-          <ul class="btn-game">
-            <li>
-              <button
-                type="button"
-                class="btn-game__btn btn-game__red"
-                @click="pressButton('red')"
-                :class="{'btn-game__btn-active':isActive === 'red'}"
-                :disabled="disabledButton"
-              ></button>
-            </li>
-            <li>
-              <button
-                type="button"
-                class="btn-game__btn btn-game__blue"
-                @click="pressButton('blue')"
-                :class="{'btn-game__btn-active':isActive === 'blue'}"
-                :disabled="disabledButton"
-              ></button>
-            </li>
-            <li>
-              <button
-                type="button"
-                class="btn-game__btn btn-game__yellow"
-                @click="pressButton('yellow')"
-                :class="{'btn-game__btn-active':isActive === 'yellow'}"
-                :disabled="disabledButton"
-              ></button>
-            </li>
-            <li>
-              <button
-                type="button"
-                class="btn-game__btn btn-game__green"
-                @click="pressButton('green')"
-                :class="{'btn-game__btn-active':isActive === 'green'}"
-                :disabled="disabledButton"
-              ></button>
-            </li>
-          </ul>
-        </div>
-        <div class="container__option">
-          <div class="option">
-            <p class="option__round">
-              Round:
-              <span class="option__round-conter">{{ counter }}</span>
-            </p>
+    <div class="app__container-title">
+      <p class="app__title">Simon says</p>
+    </div>
+    <div class="container">
+      <div class="container__btn">
+        <ul class="btn-game">
+          <li>
             <button
               type="button"
-              class="option__btn"
-              @click="startGame"
-              :disabled="disabledStart"
-            >Start</button>
-            <p v-if="alertGameOver" class="option__game-over">Game over</p>
-            <p v-if="alertCongratulation" class="option__game-complete">You Win!</p>
-            <p class="opion__title-list-option">Game options:</p>
-            <ul class="option__list">
-              <li class="opton__item">
-                <input type="radio" name="level" id="easy" value="1500" v-model="mode" />
-                <label for="easy">Easy</label>
-              </li>
-              <li class="opton__item">
-                <input type="radio" name="level" id="middle" value="1000" v-model="mode" />
-                <label for="middle">Middle</label>
-              </li>
-              <li class="opton__item">
-                <input type="radio" name="level" id="hard" value="400" v-model="mode" />
-                <label for="hard">Hard</label>
-              </li>
-            </ul>
-          </div>
+              class="btn-game__btn btn-game__red"
+              @click="pressButton('red')"
+              :class="{'btn-game__btn-active':isActiveRed}"
+            ></button>
+          </li>
+          <li>
+            <button
+              type="button"
+              class="btn-game__btn btn-game__blue"
+              @click="pressButton('blue')"
+              :class="{'btn-game__btn-active':isActiveBlue}"
+            ></button>
+          </li>
+          <li>
+            <button
+              type="button"
+              class="btn-game__btn btn-game__yellow"
+              @click="pressButton('yellow')"
+              :class="{'btn-game__btn-active':isActiveYellow}"
+            ></button>
+          </li>
+          <li>
+            <button
+              type="button"
+              class="btn-game__btn btn-game__green"
+              @click="pressButton('green')"
+              :class="{'btn-game__btn-active':isActiveGreen}"
+            ></button>
+          </li>
+        </ul>
+      </div>
+      <div class="container__option">
+        <div class="option">
+          <p class="option__round">
+            Round:
+            <span class="option__round-conter">{{ counter }}</span>
+          </p>
+          <button type="button" class="option__btn" @click="startGame" :disabled="disabled">Start</button>
+          <p v-if="alertGameOver" class="option__game-over">Game over</p>
+          <p class="opion__title-list-option">Game options:</p>
+          <ul class="option__list">
+            <li class="opton__item">
+              <input type="radio" name="level" id="easy" value="1500" v-model="mode" />
+              <label for="easy">Easy</label>
+            </li>
+            <li class="opton__item">
+              <input type="radio" name="level" id="middle" value="1000" v-model="mode" />
+              <label for="middle">Middle</label>
+            </li>
+            <li class="opton__item">
+              <input type="radio" name="level" id="hard" value="400" v-model="mode" />
+              <label for="hard">Hard</label>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
@@ -83,17 +71,16 @@
 
 <script>
 export default {
-  components: {},
-
   data() {
     return {
-      disabledButton: true,
-      disabledStart: false,
-      isActive: "",
+      disabled: false,
+      isActiveRed: false,
+      isActiveBlue: false,
+      isActiveYellow: false,
+      isActiveGreen: false,
       alertGameOver: false,
-      alertCongratulation: false,
       counter: 0,
-      lengthQueue: 20,
+      lengthQueue: 3,
       allQueue: [],
       mode: 1500,
     };
@@ -101,15 +88,14 @@ export default {
 
   methods: {
     pressButton(userColor) {
-      if (this.disabledButton) {
+      if (!this.disabled) {
         return;
-      } else if (this.allQueue.length > 0) {
+      }
+      if (this.allQueue.length > 0) {
         let color = this.allQueue.shift();
-        this.flickerButton(userColor);
         if (color !== userColor) {
           this.gameOver();
         } else if (this.allQueue.length === 0) {
-          this.disabledButton = true;
           this.levelComplete();
         }
       } else {
@@ -120,13 +106,13 @@ export default {
     gameOver() {
       this.counter = 0;
       this.allQueue = [];
-      this.disabledStart = false;
-      this.disabledButton = true;
+      this.disabled = false;
       this.alertGameOver = true;
+      console.log("Игра окончена");
     },
 
     levelComplete() {
-      if (this.counter !== this.lengthQueue) {
+      if (this.counter != this.lengthQueue) {
         this.counter++;
         this.allQueue = [];
         this.randomColor();
@@ -134,21 +120,22 @@ export default {
       } else {
         this.counter = 0;
         this.allQueue = [];
-        this.disabledStart = false;
-        this.alertCongratulation = true;
+        this.disabled = false;
+        alert("You win!!");
       }
     },
 
     randomColor() {
-      for (let i = 0; i < this.counter; i++) {
+      let nextLevel = this.counter;
+      for (let i = 0; i < nextLevel; i++) {
         this.createQueue();
       }
     },
 
     startGame() {
-      this.alertCongratulation = false;
+      console.log(this.mode);
       this.alertGameOver = false;
-      this.disabledStart = true;
+      this.disabled = true;
       this.counter++;
       this.createQueue();
       this.playQueue([...this.allQueue]);
@@ -171,8 +158,6 @@ export default {
       this.flickerButton(arrayElement);
       if (array.length > 0) {
         setTimeout(this.playQueue, this.mode, array);
-      } else {
-        this.disabledButton = false;
       }
     },
 
@@ -182,13 +167,37 @@ export default {
     },
 
     highlightButton(el) {
-      this.isActive = el;
-      let audio = new Audio(require(`../public/sound/${el}.mp3`));
-      audio.play();
+      switch (el) {
+        case "red":
+          this.isActiveRed = true;
+          break;
+        case "blue":
+          this.isActiveBlue = true;
+          break;
+        case "yellow":
+          this.isActiveYellow = true;
+          break;
+        case "green":
+          this.isActiveGreen = true;
+          break;
+      }
     },
 
     colorReturn(el) {
-      this.isActive = "";
+      switch (el) {
+        case "red":
+          this.isActiveRed = false;
+          break;
+        case "blue":
+          this.isActiveBlue = false;
+          break;
+        case "yellow":
+          this.isActiveYellow = false;
+          break;
+        case "green":
+          this.isActiveGreen = false;
+          break;
+      }
     },
   },
 };
@@ -253,12 +262,6 @@ export default {
   font-size: 20px
   font-weight: 700
   color: #ff0000
-  margin-bottom: 5px
-
-.option__game-complete
-  font-size: 20px
-  font-weight: 700
-  color: #00ff7f
   margin-bottom: 5px
 
 .option__round
